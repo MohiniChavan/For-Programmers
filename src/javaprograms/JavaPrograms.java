a21a9260473817384cb47d76c4d0c7f4345f2125
mohini <mohini@DESKTOP-II0D4I8>
2020-01-03 13:45:09 +0530

    Java First Program

---
+++ src/javaprograms/JavaPrograms.java
@@ -0,0 +1,16 @@
+/*
+Java First Hello World Program
+ */
+package javaprograms;
+
+/**
+ *
+ * @author mohini
+ */
+public class JavaPrograms {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
